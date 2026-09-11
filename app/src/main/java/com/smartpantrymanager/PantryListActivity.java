5b0577dae210188e44bff1fe7a35826b7b84293d
Shellah Gwenzi <sgwenzi13@gmail.com>
2026-09-11 21:07:23 +0200

    Refine PantryListActivity logic and layout

--- app/src/main/java/com/smartpantrymanager/PantryListActivity.java
+++ app/src/main/java/com/smartpantrymanager/PantryListActivity.java
@@ -1,4 +1,13 @@
-package com.smartpantrymanager;
+package com.example.myapplication;
 
-public class PantryListActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PantryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantry_list);
+    }
 }
